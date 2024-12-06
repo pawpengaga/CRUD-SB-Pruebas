0b0e0dc2bcff151bd79400cdab12ff68ac37d09e
Erick Rivera <yomellamoerick.771@gmail.com>
2024-12-05 21:35:54 -0300

    the DAO update

--- src/main/java/com/pawpengaga/main/controlador/ControladorDummy.java
+++ src/main/java/com/pawpengaga/main/controlador/ControladorDummy.java
@@ -3,6 +3,8 @@ package com.pawpengaga.main.controlador;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -10,18 +12,21 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.pawpengaga.main.dao.DvdDAO;
 import com.pawpengaga.main.modelo.Dvd;
 
 @Controller
 @RequestMapping("/dvd")
 public class ControladorDummy {
 
-  List<Dvd> listadoDvd = new ArrayList<>();
+  @Qualifier("dvddao")
+  @Autowired
+  DvdDAO dvdService;
 
   @GetMapping("/listar")
   public ModelAndView inicio(){
     ModelAndView mav = new ModelAndView("listadoDvd");
-    mav.addObject("listado", listadoDvd); // Un simil de .setAttribute() de los Servlets
+    mav.addObject("listado", dvdService.mostrarDvd()); // Un simil de .setAttribute() de los Servlets
     return mav;
   }
 
@@ -33,7 +38,7 @@ public class ControladorDummy {
   // Cuando pensemos en modelos hay que pensar en objetos Java
   @PostMapping("/grabar")
   public String grabarDvd(@ModelAttribute("Dvd") Dvd dvd){
-    listadoDvd.add(dvd);
+    dvdService.grabar(dvd);
     return "redirect:/dvd/listar";
   }
 
--- src/main/java/com/pawpengaga/main/dao/DvdDAO.java
+++ src/main/java/com/pawpengaga/main/dao/DvdDAO.java
@@ -1,5 +1,26 @@
 package com.pawpengaga.main.dao;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import com.pawpengaga.main.modelo.Dvd;
+
+@Component("dvddao")
 public class DvdDAO {
+
+  @Qualifier("listadoDvd") // Nos traermos la lista del contexto general
+  @Autowired
+  private List<Dvd> listadoDvd;
+
+  public void grabar(Dvd dvd){
+    listadoDvd.add(dvd);
+  }
+
+  public List<Dvd> mostrarDvd(){
+    return listadoDvd;
+  }
 
 }
